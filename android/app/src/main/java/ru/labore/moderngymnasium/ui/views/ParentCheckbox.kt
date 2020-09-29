4f1c245bbba195afed06d55fef4ddd9b32bdaf30
Kimp13 <akunec41@gmail.com>
2020-09-29 22:30:34 +0400

    дополняем parent checkbox.

--- android/app/src/main/java/ru/labore/moderngymnasium/ui/views/ParentCheckbox.kt
+++ android/app/src/main/java/ru/labore/moderngymnasium/ui/views/ParentCheckbox.kt
@@ -2,16 +2,22 @@ package ru.labore.moderngymnasium.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.core.view.children
 import kotlinx.android.synthetic.main.activity_login.view.*
 import ru.labore.moderngymnasium.R
 
-class ParentCheckbox : androidx.appcompat.widget.AppCompatCheckBox {
+class ParentCheckbox : ViewGroup {
     companion object {
         private const val UNKNOWN = -1
         private const val UNCHECKED = 0
         private const val CHECKED = 1
     }
 
+    private var checkedChangeHandler: ((Int) -> Unit)? = null
+    private lateinit var checkedChildren: MutableList<View>
     private var state = 0
         set(value) {
             field = value
@@ -33,14 +39,41 @@ class ParentCheckbox : androidx.appcompat.widget.AppCompatCheckBox {
     }
 
     private fun init() {
-        state = UNKNOWN
+        state = CHECKED
         updateButton()
+
+        children.forEach {
+            if (it is ViewGroup) {
+                (it as ParentCheckbox).setOnCheckedChangeListener { childState ->
+                    if (childState == CHECKED) {
+                        if (checkedChildren.indexOf(it) == -1) {
+                            checkedChildren.add(it)
+                        }
+                    } else {
+                        checkedChildren.remove(it)
+                    }
+                }
+            } else {
+                (it as CheckBox).setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        if (checkedChildren.indexOf(it) == -1) {
+                            checkedChildren.add(it)
+                        }
+                    } else {
+                        checkedChildren.remove(it)
+                    }
+                }
+            }
+        }
         
-        setOnCheckedChangeListener { _, _ ->
+        setOnClickListener {
             state = when(state) {
-                UNKNOWN -> UNCHECKED
-                UNCHECKED -> CHECKED
-                else -> UNKNOWN
+                CHECKED -> UNCHECKED
+                else -> CHECKED
+            }
+
+            checkedChangeHandler?.let {
+                it(state)
             }
 
             updateButton()
@@ -48,6 +81,24 @@ class ParentCheckbox : androidx.appcompat.widget.AppCompatCheckBox {
     }
 
     private fun updateButton() {
-        
+
+        if (state == CHECKED) {
+            checkedChildren = mutableListOf()
+
+            children.forEach {
+                checkedChildren.add(it)
+                (it as CheckBox).isChecked = true
+            }
+        } else if (state == UNCHECKED) {
+            checkedChildren = mutableListOf()
+        }
+    }
+
+    fun setOnCheckedChangeListener(lambda: (Int) -> Unit) {
+        checkedChangeHandler = lambda
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        TODO("Not yet implemented")
     }
 }
